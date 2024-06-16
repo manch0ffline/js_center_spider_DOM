4413b25713bff617ecca8c182549093e30a27f34
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2024-06-16 16:19:06 +0200

    make build

---
+++ index.c9d4e0ff.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.left="".concat(e.clientWidth/2-t.clientHeight/2,"px"),t.style.top="".concat(e.clientHeight/2-t.clientWidth/2,"px");
+//# sourceMappingURL=index.c9d4e0ff.js.map
